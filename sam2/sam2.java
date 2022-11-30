4191e653c99178bd42b4947e8b51c9a6f71ed3f4
Drei4 <antipin-lukinf@mail.ru>
2022-11-30 22:48:38 +0300

    add ne rabotaet

--- sam2/sam2.java
+++ sam2/sam2.java
@@ -1,24 +1,28 @@
 // Дана последовательность чисел. Получить список неповторяющихся элементов исходной последовательности
 // Пример: [1, 2, 3, 5, 1, 5, 3, 10] => [1, 2, 3, 5, 10]
-
+//import org.apache.commons.lang3.ArrayUtils;
+ 
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class sam2 {
     public static void main(String[] args) {
-        int k = 0;
-        int j = 0;
-        int[] subseq = new int[] {1, 5, 8, 9, 4, 6, 5, 1};  
-        //duplicates=false;
-        for (j=0; j<subseq.length; j++)
-            if (subseq[k] != subseq[j])
-                System.out.print(subseq[j]);
-            for (k=j; k<subseq.length;k++){
-                System.out.print(k);
-
-                //if (subseq[k] != subseq[j])
-                    //System.out.print(subseq[k]);
+                
+        int[] subseq = new int[] {1, 5, 8, 9, 4, 6, 5, 1};
+        for(int i=0; i<subseq.length; i++) {
+            for (int j=i+1; j<subseq.length; j++) {
+                if(subseq[i] == subseq[j]) {
+                    
+                                                  
+                }
+                
             }
+        }        
+
+                
+    }
                     
         
-    }
-    
 }
+    
+
